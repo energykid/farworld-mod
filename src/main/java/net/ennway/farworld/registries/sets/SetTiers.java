c8c4c48f7a9fb50e52c39a7092c003c2247fbb1f
Xhack <72313282+energykid@users.noreply.github.com>
2025-09-23 19:52:14 -0400

    some bystone-related advancements

--- src/main/java/net/ennway/farworld/registries/sets/SetTiers.java
+++ src/main/java/net/ennway/farworld/registries/sets/SetTiers.java
@@ -1,9 +1,14 @@
 package net.ennway.farworld.registries.sets;
 
+import net.ennway.farworld.Farworld;
 import net.ennway.farworld.registries.ModItems;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.SimpleTier;
 import net.neoforged.neoforge.common.Tags;
 
@@ -17,8 +22,12 @@ public class SetTiers {
             () -> Ingredient.of(ModItems.SOUL_STEEL)
     );
 
+    public static final TagKey<Block> NEEDS_COBALT_TOOL = TagKey.create(
+            BuiltInRegistries.BLOCK.key(),
+            ResourceLocation.fromNamespaceAndPath(Farworld.MOD_ID, "needs_cobalt_tool"));
+
     public static final Tier COBALT_TIER = new SimpleTier(
-            BlockTags.NEEDS_DIAMOND_TOOL,
+            NEEDS_COBALT_TOOL,
             450,
             8.0F,
             3.0F,
