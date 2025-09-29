fd0f5c8f7d544b168ddcd373571a148c4e237c66
ENNWAY <72313282+energykid@users.noreply.github.com>
2025-09-29 11:16:08 -0400

    Lush foliage, needs flowering and tall variants

--- src/main/java/net/ennway/farworld/block/Gloomcap.java
+++ src/main/java/net/ennway/farworld/block/Gloomcap.java
@@ -1,16 +1,24 @@
 package net.ennway.farworld.block;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.RootsBlock;
 import net.minecraft.world.level.block.TallGrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class Gloomcap extends TallGrassBlock {
+public class Gloomcap extends BushBlock {
     public Gloomcap(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends BushBlock> codec() {
+        return simpleCodec(Gloomcap::new);
     }
 
     protected static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 10.0, 12.0);
--- src/main/java/net/ennway/farworld/block/LushFoliage.java
+++ src/main/java/net/ennway/farworld/block/LushFoliage.java
@@ -1,16 +1,26 @@
 package net.ennway.farworld.block;
 
+import com.mojang.serialization.MapCodec;
+import net.ennway.farworld.registries.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.RootsBlock;
 import net.minecraft.world.level.block.TallGrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class LushFoliage extends TallGrassBlock {
+public class LushFoliage extends BushBlock {
     public LushFoliage(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends BushBlock> codec() {
+        return simpleCodec(LushFoliage::new);
     }
 
     protected static final VoxelShape SHAPE = Block.box(4.0, 0.0, 4.0, 12.0, 10.0, 12.0);
@@ -18,5 +28,11 @@ public class LushFoliage extends TallGrassBlock {
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        BlockState st = level.getBlockState(pos.below());
+        return st.is(ModBlocks.LUSH_FLOWSTONE);
     }
 }
